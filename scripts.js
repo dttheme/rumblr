1067346541cc8cce1335acb0bcd9c44b8c2b7356
dttheme <dttheme@gmail.com>
2017-11-26 21:46:47 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+
+
+//pull data from WebGL JS API (http://www.webglearth.org/api)
+
+//pull data from USGS; coordinates, name, time/date, magnitude
+
+//pull tweets from Twitter using coordinates and #earthquake
+//stretch: show Places using coordinates
