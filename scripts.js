fe088daad22a57d92b59e0b4614a26e7606e3fce
dttheme <dttheme@gmail.com>
2017-11-28 19:04:04 -0500

    added new HTML, CSS and made button clicks functional

--- scripts.js
+++ scripts.js
@@ -1,63 +1,83 @@
 
+let numberOfDaysSelected;
 
+//when the user submits, the landing page will disappear and the earth will be rendered
+function takeUserInputRevealEarth() {
+	$('.js-submit-button').click(function(event) {
+		$('.landing-page').addClass('hidden');
+		$('.left-section').removeClass('hidden');
+		$('.right-section').removeClass('hidden');
+		numberOfDaysSelected = $('#myRange').val();
+		console.log(numberOfDaysSelected);
+		initialize();
+	})
+}
+takeUserInputRevealEarth();
 
 //pull data from WebGL JS API (http://www.webglearth.org/api)
 function initialize() {
-        var options = {
-        	atmosphere: true, 
-        	center: [0, 0], 
-        	zoom: 3,
-        };
+	var options = {
+		atmosphere: true, 
+		sky: true,
+		center: [0, 0], 
+		zoom: 0,
+	};
         //rotate Earth
         var before = null;
         requestAnimationFrame(function animate(now) {
-            var c = earth.getPosition();
-            var elapsed = before? now - before: 0;
-            before = now;
-            earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
-            requestAnimationFrame(animate);
+        	var c = earth.getPosition();
+        	var elapsed = before? now - before: 0;
+        	before = now;
+        	earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+        	requestAnimationFrame(animate);
         });
         //make Earth
         var earth = new WE.map('earth_div', options);
         WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
-          minZoom: 0,
-          maxZoom: 5,
-          attribution: 'NASA'
+        	minZoom: 0,
+        	maxZoom: 5,
+        	attribution: 'NASA'
         }).addTo(earth);
-      }
+    }
 
-      //create markers with USGS coords
-      //use .addTo(map), .setView([x,y],z), WE.marker, marker.bindPopup(html)
+//takes in coordinate object and puts them on the map
+function populateMarkers() {
+	//coordinates from USGS data
+    //use .addTo(map), .setView([x,y],z), WE.marker, marker.bindPopup(html)
+    //reference API demo: http://examples.webglearth.com/#apidemo
+}
 
-      // function addSomeMarkers() {
-      //   document.getElementById('addmarkers').disabled = true;
+//when the marker is clicked, or when the user clicks a location in the list, the earth will pan to that marker
+function panToMarker() {};
 
-      //   map.setView([51.505, 0], 5);
-      //   var marker = WE.marker([51.5, -0.09]).addTo(map);
-      //   marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
-
-      //   var marker2 = WE.marker([30.058056, 31.228889]).addTo(map);
-      //   marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!<br />Here, enjoy another polygon..", {maxWidth: 120, closeButton: false});
-
-      //pan to coords
+//listens to the user input, updates the DOM
+function displayNumberOfDays() {
+	$(document).on('input', '#myRange', function() {
+    $('#numberOfDays').html( $(this).val() );
+});
+}
+displayNumberOfDays();
 
 //pull data from USGS; coordinates, name, time/date, magnitude
 
-	//create div with coords, name, t/d, mag written inside
-	const USGS_EARTHQUAKE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
-function pullMostRecentEarthquakeData() {
-	var date = Date();
-	console.log(date);
+//create div with coords, name, t/d, mag written inside
+const USGS_EARTHQUAKE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
+function earthquakeDataFromApi() {
+		const query = {
+			starttime: '2017-11-28',
+			endtime: '2017-11-28',
+			minmagnitude: 1,
+		}
+		// $('.js-submit-button').click(function(event) {
+			$.getJSON(USGS_EARTHQUAKE_URL, query, function(data){
+			console.log(data);
+		})
+	// })
 }
+earthquakeDataFromApi();
 
 
 
 //pull tweets from Twitter using coordinates and #earthquake
 
 	//Twitter API Key: 	S1heh865MzpqXkBaCF6lrbo7p
-
-
-
-
-
-//stretch: show Places using coordinates
