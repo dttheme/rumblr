ca6a5ed611e43985c2b85bfb9eab69915ef95d95
dttheme <dttheme@gmail.com>
2017-11-29 13:38:55 -0500

    fixing date issues

--- scripts.js
+++ scripts.js
@@ -1,18 +1,36 @@
 
-let numberOfDaysSelected = 1;
+function submitDateToApi() {
+	$('.js-submit-button').click(function(event) {
+		let today = new Date();
+		let numberOfDaysSelected = $('#myRange').val();
+		let dateInPast = (findDateInPast(today, numberOfDaysSelected));
+		function createDateForUrl(d) {
+			let dateInISO = (d.toISOString());
+			let dateForURL = dateInISO.slice(0,10);
+			console.log(dateInISO);
+			console.log(dateForURL);
+			return dateForURL;
+		}
+		console.log(today);
+		let endTimeUrlString = createDateForUrl(today);
+		console.log(numberOfDaysSelected);
+		console.log(dateInPast);
+		let startTimeUrlString = createDateForUrl(dateInPast);
+		console.log(startTimeUrlString);
+	})
+}
+
 
 //when the user submits, the landing page will disappear and the earth will be rendered
-function takeUserInputRevealEarth() {
+function revealEarth() {
 	$('.js-submit-button').click(function(event) {
 		$('.landing-page').addClass('hidden');
 		$('.left-section').removeClass('hidden');
 		$('.right-section').removeClass('hidden');
-		numberOfDaysSelected = $('#myRange').val();
-		console.log(numberOfDaysSelected);
 		initialize();
 	})
 }
-takeUserInputRevealEarth();
+revealEarth();
 
 //pull data from WebGL JS API (http://www.webglearth.org/api)
 function initialize() {
@@ -40,28 +58,47 @@ function initialize() {
         }).addTo(earth);
     }
 
+
+
+//take user day input, count backwards to find past day, transform date for url
+function findDateInPast(date, days) {
+	return new Date(
+		date.getFullYear(),
+		date.getMonth(),
+		date.getDate() - days
+		);
+}
+
+
+
+
 //create div with coords, name, t/d, mag written inside
 const USGS_EARTHQUAKE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
 function earthquakeDataFromApi() {
-		const query = {
-			starttime: createDateForUrl(dateInPast),
-			endtime: createDateForUrl(date),
+	submitDateToApi();
+	// console.log(startTimeUrlString)
+	const query = {
+			// starttime: startTimeUrlString,
 			minmagnitude: 1,
 		};
 		$('.js-submit-button').click(function(event) {
 			$.getJSON(USGS_EARTHQUAKE_URL, query, function(data){
-			console.log(data);
+				console.log(data);
+			})
 		})
-	})
-}
-earthquakeDataFromApi();
+	}
+	earthquakeDataFromApi();
 
 //takes in coordinate object and puts them on the map
 function renderMarkers() {
 	//coordinates from USGS data
-	console.log(${})
+	// console.log(${})
     //use .addTo(map), .setView([x,y],z), WE.marker, marker.bindPopup(html)
     //reference API demo: http://examples.webglearth.com/#apidemo
+//     for (i=0; i<= numberOfDaysSelected; i++) {
+//     	var marker = WE.marker([-41,21]).addTo(earth);
+//     	marker.bindPopup('<p>Hey there!</p>')
+//     }
 }
 
 //when the marker is clicked, or when the user clicks a location in the list, the earth will pan to that marker
@@ -70,26 +107,13 @@ function panToMarker() {};
 //listens to the user input, updates the DOM
 function displayNumberOfDays() {
 	$(document).on('input', '#myRange', function() {
-    $('#numberOfDays').html( $(this).val() );
-});
+		$('#numberOfDays').html( $(this).val() );
+	});
 }
 displayNumberOfDays();
 
 
-//take user day input, count backwards to find past day, transform date for url
-let date = new Date();
-function findDateInPast(date, days) {
-	return new Date(
-		date.getFullYear(),
-		date.getMonth(),
-		date.getDate() - days
-		);
-}
-let dateInPast = (findDateInPast(date, numberOfDaysSelected));
-function createDateForUrl(d) {
-	let dateInISO = (d.toISOString());
-	let dateForURL = dateInISO.slice(0,9);
-}
+
 
 //pull data from USGS; coordinates, name, time/date, magnitude
 
