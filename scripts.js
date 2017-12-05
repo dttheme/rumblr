dbdedbd19f273c437fc15c132e5512e37eb72619
dttheme <dttheme@gmail.com>
2017-12-05 18:47:53 -0500

    added total earthquakes; made sidebar more responsive

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 
 $('.js-submit-button').click(function(event) {	
+
 	function createDateForUrl(date) {
 		let dateInISO = (date.toISOString());
 		let dateForURL = dateInISO.slice(0,10);
@@ -17,7 +18,7 @@ $('.js-submit-button').click(function(event) {
 		return	startTimeUrlString;
 	}
 
-	//when the user submits, the landing page will disappear and the earth will be rendered
+//when the user submits, the landing page will disappear and the earth will be rendered
 	function revealEarth() {
 		$('.landing-page').addClass('hidden');
 		$('.left-section').removeClass('hidden');
@@ -25,8 +26,7 @@ $('.js-submit-button').click(function(event) {
 		initialize();
 	}
 
-// ------
-	//pull data from WebGL JS API (http://www.webglearth.org/api)
+//pull data from WebGL JS API (http://www.webglearth.org/api)
 	function initialize() {
 		var options = {
 			atmosphere: true, 
@@ -36,16 +36,18 @@ $('.js-submit-button').click(function(event) {
 			zooming: true
 
 		};
-	        // //rotate Earth
-	        // var before = null;
-	        // requestAnimationFrame(function animate(now) {
-	        // 	var c = earth.getPosition();
-	        // 	var elapsed = before? now - before: 0;
-	        // 	before = now;
-	        // 	earth.setCenter([c[0], c[1] + 0.1*(elapsed/300)]);
-	        // 	requestAnimationFrame(animate);
-	        // });
-	        //make Earth
+
+//rotate Earth
+	        var before = null;
+	        requestAnimationFrame(function animate(now) {
+	        	var c = earth.getPosition();
+	        	var elapsed = before? now - before: 0;
+	        	before = now;
+	        	earth.setCenter([c[0], c[1] + 0.1*(elapsed/500)]);
+	        	requestAnimationFrame(animate);
+	        });
+
+//make Earth
 	        var earth = new WE.map('earth_div', options);
 	        WE.tileLayer('https://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
 	        	minZoom: 0,
@@ -53,7 +55,7 @@ $('.js-submit-button').click(function(event) {
 	        	attribution: 'NASA'
 	        }).addTo(earth);
 
-	        //create div with coords, name, t/d, mag written inside
+//create div with coords, name, t/d, mag written inside
 	        const USGS_EARTHQUAKE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
 	        function earthquakeDataFromApi() {
 	        	console.log('submitDateToApi output: ' + submitDateToApi());
@@ -65,18 +67,20 @@ $('.js-submit-button').click(function(event) {
 	        	};
 	        	$.getJSON(USGS_EARTHQUAKE_URL, query, function(data){
 	        		let JSONdata = JSON.stringify(data, null, 2);
-					// console.log(JSONdata);
+					console.log(JSONdata);
 					for (i=0; i < data.features.length; i++) {
 						let features = (data.features[i]);
 						renderMarkers(features);	
 						renderList(features);
 					}
+					renderTotalEarthquakes(data);
+
 
 				});
 	        }
-			//takes in coordinate object and puts them on the map
+
+//renders markers on map using coordinates
 			function renderMarkers(features) {
-				// console.log(features.geometry.coordinates);
 				epochDate = features.properties.time;
 				humanDate = new Date(epochDate);
 				var marker = WE.marker([features.geometry.coordinates[1], features.geometry.coordinates[0]], 'pin.png', 32, 32).addTo(earth);
@@ -87,13 +91,10 @@ $('.js-submit-button').click(function(event) {
 					<!--Coordinates: ${features.geometry.coordinates[1], features.geometry.coordinates[0]}-->
 					</p>`)
 			}
-			    // // //when the marker is clicked, or when the user clicks a location in the list, the earth will pan to that marker
-			    // function panToMarker(features) {
-			    // 	marker.click(function(event) {
-			    // 		map.panTo([features.geometry.coordinates[1], features.geometry.coordinates[0]]);
-			    // 	});
-			    // }
-			    function renderList(features) {
+			
+
+//creates a side bar featuring details
+			  function renderList(features) {
 			    	earthquakeDataDiv = 
 			    	`<div class='dataDiv'>
 			    		<p>
@@ -101,33 +102,40 @@ $('.js-submit-button').click(function(event) {
 							<b>Date:</b> ${humanDate} <br>
 							<b>Magnitude:</b> ${features.properties.mag} <br>
 						</p>
+						<button class='travelButton'>Go!</button>
 						</div>
 			    		`;
 			    	$('.left-section').append(earthquakeDataDiv)
+			    	
 			    }
+//give a count of the total earthquakes on the sidebar
+			    function renderTotalEarthquakes(data) {
+			    	$('.totalEarthquakes').html(
+			    	`<p style='font-size=20px;'><b>Total Earthquakes: ${data.metadata.count}</b></p>`
+			    	)
+			    }
+
 			    earthquakeDataFromApi();
+
+}
+
+//take user day input, count backwards to find past day, transform date for url
+			function findDateInPast(date, days) {
+				return new Date(
+					date.getFullYear(),
+					date.getMonth(),
+					date.getDate() - days
+					)
 			}
-// ------
-
-
-	//take user day input, count backwards to find past day, transform date for url
-	function findDateInPast(date, days) {
-		return new Date(
-			date.getFullYear(),
-			date.getMonth(),
-			date.getDate() - days
-			)
-	}
 	
-//pull data from USGS; coordinates, name, time/date, magnitude
 
-//pull tweets from Twitter using coordinates and #earthquake
-
-	//Twitter API Key: 	S1heh865MzpqXkBaCF6lrbo7p
 
 	revealEarth();
 });
  //end submit button actions
+
+
+
 
 
 //listens to the user input, updates the DOM
@@ -143,3 +151,9 @@ function displayMinimumMagnitude() {
 }
 displayMinimumMagnitude();
 displayNumberOfDays();
+
+//pull data from USGS; coordinates, name, time/date, magnitude
+
+//pull tweets from Twitter using coordinates and #earthquake
+
+	//Twitter API Key: 	S1heh865MzpqXkBaCF6lrbo7p
